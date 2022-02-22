ec3412c7ca0295fa9e186073a4e64a984d9d8e87
toad sage <shankarprakash32@gmail.com>
2022-02-22 14:55:34 +0530

    changed the artifactId and groupId

--- src/main/resources/META-INF/resources/frontend/styles/step-header-styles.js
+++
@@ -1,8 +0,0 @@
-import styles from './step-header-styles.css'
-
-const $_documentContainer = document.createElement('template');
-$_documentContainer.innerHTML = `
-  <dom-module id="step-header-styles">
-    <template><style>${styles}</style></template>
-  </dom-module>`;
-document.head.appendChild($_documentContainer.content);
--- src/main/resources/META-INF/resources/frontend/styles/stepper-styles.js
+++
@@ -1,8 +0,0 @@
-import styles from './stepper-styles.css'
-
-const $_documentContainer = document.createElement('template');
-$_documentContainer.innerHTML = `
-  <dom-module id="stepper-styles">
-    <template><style>${styles}</style></template>
-  </dom-module>`;
-document.head.appendChild($_documentContainer.content);
